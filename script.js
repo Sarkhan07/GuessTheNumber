9e997584d01a40e0a08ce5ae18bf7aff13135f86
Sarkhan <abdullayevsarxan92@gmail.com>
2023-03-23 01:31:43 +0400

    feat: validation for inputs

--- script.js
+++ script.js
@@ -6,30 +6,67 @@ function startGame() {
     startBtn.addEventListener('click', function () {
         overlay.style.display = 'flex';
     });
-
-    cancel.addEventListener('click', function () {
-        overlay.style.display = 'none';
-    });
 }
 startGame();
+
+function close() {
+    let cancel = document.querySelector('.cancel-game');
+
+    document.querySelector('.overlay').style.display = 'none';
+
+    cancel.addEventListener('click', function () {
+        document.querySelector('.overlay').style.display = 'none';
+        document.querySelector('.init-title').innerHTML =
+            'Введите диапазон чисел чтобы я мог загадать число';
+        document.querySelector('.init-title').style.color =
+            'rgba(0, 0, 0, 0.5)';
+        let minInput = document.querySelector('.init-input_1'),
+            maxInput = document.querySelector('.init-input_2');
+        minInput.value = 0;
+        maxInput.value = 0;
+    });
+}
+
+close();
 
 function getValue() {
     let minNum = document.querySelector('.min'),
         maxNum = document.querySelector('.max'),
         minInput = document.querySelector('.init-input_1'),
         maxInput = document.querySelector('.init-input_2'),
+        title = document.querySelector('.init-title'),
         maxValue,
         minValue;
 
     minInput.addEventListener('change', function () {
         minValue = minInput.value;
+        if (minValue < 0 || minValue % 1 !== 0) {
+            title.innerHTML =
+                'Вы ввели неверный диапазон, введите пожалуйста только целые и положительные числа!';
+            title.style.color = 'red';
+            title.style.fontSize = '20px';
+            minValue = 0;
+            document.querySelector('.game-title').innerHTML =
+                'Вы ввели неверный диапазон, введите пожалуйста только целые и положительные числа!';
+        }
         minNum.innerHTML = minValue;
         updateRandomNum();
+        close();
     });
     maxInput.addEventListener('change', function () {
         maxValue = maxInput.value;
+        if (maxValue < 0 || maxValue % 1 !== 0) {
+            title.innerHTML =
+                'Вы ввели неверный диапазон, нельзя указать отрицательное и нецелое число!';
+            title.style.color = 'red';
+            title.style.fontSize = '20px';
+            maxValue = 0;
+            document.querySelector('.game-title').innerHTML =
+                'Вы ввели неверный диапазон, введите пожалуйста только целые и положительные числа!';
+        }
         maxNum.innerHTML = maxValue;
         updateRandomNum();
+        close();
     });
 
     function updateRandomNum() {
